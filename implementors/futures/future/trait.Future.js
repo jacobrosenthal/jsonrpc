1a86a401d245a266354890c1140dc9e2b54ffb28
Travis CI User <travis@example.org>
2019-03-12 10:01:54 +0000

    Update documentation

---
+++ implementors/futures/future/trait.Future.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["jsonrpc_pubsub"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://docs.rs/futures/0.1/futures/future/trait.Future.html\" title=\"trait futures::future::Future\">Future</a> for <a class=\"struct\" href=\"jsonrpc_pubsub/oneshot/struct.Receiver.html\" title=\"struct jsonrpc_pubsub::oneshot::Receiver\">Receiver</a>&lt;T&gt;",synthetic:false,types:["jsonrpc_pubsub::oneshot::Receiver"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
